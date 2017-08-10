91fff1fc0e64a9b150fdbc3fb5b1a8953d99e3ab
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-08-10 15:13:30 -0300

    Add vector2 data struct

--- vector2.go
+++ vector2.go
@@ -1,6 +1,44 @@
 package main
 
 type Vector2 struct {
-	x int
-	y int
+	x float64
+	y float64
+}
+
+func (this *Vector2) x() {
+	return this.x
+}
+
+func (this *Vector2) y() {
+	return this.y
+}
+
+func (this *Vector2) set(x, y float64) {
+	this.x = x
+	this.y = y
+}
+
+func (this *Vector2) add(xy Vector2) {
+	this.x += xy.x
+	this.y += xy.y
+}
+
+func (this *Vector2) multiply(xy Vector2) {
+	this.x *= xy.x
+	this.y *= xy.y
+}
+
+func (this *Vector2) subtract(xy Vector2) {
+	this.x -= xy.x
+	this.y -= xy.y
+}
+
+func (this *Vector2) divide(xy Vector2) {
+	this.x /= xy.x
+	this.y /= xy.y
+}
+
+func (this *Vector2) reset() {
+	this.x = 0
+	this.y = 0
 }
